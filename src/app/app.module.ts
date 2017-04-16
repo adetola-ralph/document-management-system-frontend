614fb4a94b1d7c72b9896305fc1c2959b0e73e17
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-04-16 08:23:17 +0100

    ft: add router module and configure routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  { path: 'auth', component: null }, // signin and signup
+  { path: 'home', component: null }, // for viewing all the documents
+  { path: 'setting', component: null }, // user profiles
+  { path: 'documents/create', component: null}, // create a new document
+  { path: 'documents/:id', component: null}, // viewing a document
+  { path: 'documents/:id/edit', component: null}, // editing a document
+  { path: '**', component: null}, // for all other routes that don't exist
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
