5a15055e75df43c80aeec87c58b60b2d4d1a51b6
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-04-19 16:31:32 +0100

    ft: add bindings to signin and signup forms

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
+import { SignInUserInfo, SignUpUserInfo } from './authentication.helper';
+
 @Component({
-  selector: 'auth-display',
   templateUrl: './authentication.component.html',
   styleUrls: ['./authentication.component.css']
 })
@@ -9,10 +10,40 @@ export class AuthenticationComponent {
   signup: boolean;
   errorMessage: string;
   ifError: boolean;
+  signInInfo: SignInUserInfo;
+  signUpInfo: SignUpUserInfo;
 
   constructor() {
     this.signup = false;
     this.errorMessage = '';
     this.ifError = false;
+    this.signInInfo = {
+      email: '',
+      password: ''
+    };
+    this.signUpInfo = {
+      firstname: '',
+      lastname: '',
+      username: '',
+      email: '',
+      password: ''
+    };
+  }
+
+  tabChange(event): void {
+    console.log(event);
+    if (event.index === 1) {
+      this.signup = true;
+    } else {
+      this.signup = false;
+    }
+  }
+
+  signIn(): void {
+    console.log(JSON.stringify(this.signInInfo));
+  }
+
+  signUp(): void {
+    console.log(JSON.stringify(this.signUpInfo));
   }
 }
---
+++ src/app/authentication/authentication.helper.ts
@@ -0,0 +1,12 @@
+export interface SignUpUserInfo {
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface SignInUserInfo {
+  email: string;
+  password: string;
+}
