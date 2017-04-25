38080096ba766e208967607d7c2227deec3bec0e
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-04-25 17:20:47 +0100

    ft: store user information in localstorage

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material';
-import { SpinnerModule } from 'angular2-spinner';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -15,7 +14,6 @@ import { HomeComponent } from './home.component';
     CommonModule,
     FormsModule,
     MaterialModule,
-    SpinnerModule,
     HomeRoutingModule
   ],
   exports: [
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { JwtHelper} from 'angular2-jwt';
+import { JwtHelper } from 'angular2-jwt';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
