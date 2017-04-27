3d8d46bdf54ac4cf4711cb3d3210659283468b8a
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-04-27 15:32:38 +0100

    ft: add route for viewing all documents
    - redirect '/' route to '/document' route
    - make '/document' route the feault route after logging in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home.component';
 import { AuthGuard } from './services/auth-guard.service';
 
 const appRoutes: Routes = [
-  // { path: 'setting', component: null }, // user profiles
+  { path: '', redirectTo:'home', pathMatch: 'full' }, // user profiles
   // { path: 'documents/create', component: null}, // create a new document
   // { path: 'documents/:id', component: null}, // viewing a document
   // { path: 'documents/:id/edit', component: null}, // editing a document
