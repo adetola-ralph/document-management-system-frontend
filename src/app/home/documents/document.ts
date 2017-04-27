985dc02ca7045660def05adb33eff8167d17b541
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-04-27 15:34:26 +0100

    ft: document component responsible for viewing all user documents

---
+++ src/app/home/documents/document.ts
@@ -0,0 +1,11 @@
+export interface Document {
+  access: string;
+  content: string;
+  createdAt: string; // this might change
+  updateAt: string; // this also
+  documentId: string | number;
+  ownerId: string | number;
+  title: string;
+  isPublic(): boolean;
+  isOwner(userId: string | number): boolean;
+}
